2ae1c4d1bc609d372b837d2dc7d7c0ddf8f27642
魏振杰 <Kogler_S@outlook.com>
2022-09-30 13:28:11 +0800

    22w02a
    搭建初步架构

---
+++ SatoriOS/kernel/include/info.h
@@ -0,0 +1,4 @@
+extern const char* LABEL;
+extern const char* LOGO;
+extern const char* COPYRIGHT;
+extern const char* THANKS;
---
+++ SatoriOS/kernel/include/stdio.h
@@ -0,0 +1,9 @@
+extern char io_readb(unsigned long addr);
+
+extern void io_writeb(unsigned long addr, char c);
+
+extern void putc(char c);
+
+extern void puts(const char *str);
+
+extern void newline();
