22376837f999f448b0cace9b86735d439cffa516
魏振杰 <Kogler_S@outlook.com>
2022-11-29 22:36:58 +0800

    22w10d-7

--- kernel/drivers/ansi.c
+++ kernel/drivers/ansi.c
@@ -1,49 +1,40 @@
 #include "drivers/ansi.h"
 
-sint ansi_cur_color = 7;
-sint ansi_cur_style = 7;
+sint ansi_cur_color = ANSI_WHITE;
+sint ansi_cur_style = ANSI_CURSOR_STYLE_RESET;
 
 sint ansi_color_stack[ANSI_STACK_SIZE];
 sint ansi_style_stack[ANSI_STACK_SIZE];
 
+int color_stack_ptr = 0;
+int style_stack_ptr = 0;
+
 void save_cursor_style()
 {
-    static sint i = 0;
-    if (i >= ANSI_STACK_SIZE)
-    {
+    if (style_stack_ptr >= ANSI_STACK_SIZE)
         return;
-    }
-    ansi_style_stack[i++] = ansi_cur_style;
+    ansi_style_stack[style_stack_ptr++] = ansi_cur_style;
 }
 
 void restore_cursor_style()
 {
-    static sint i = 0;
-    if (i <= 0)
-    {
+    if (style_stack_ptr <= 0)
         return;
-    }
-    ansi_cur_style = ansi_style_stack[i--];
+    ansi_cur_style = ansi_style_stack[--style_stack_ptr];
     set_cursor_style(ansi_cur_style);
 }
 
 void save_cursor_color()
 {
-    static sint i = 0;
-    if (i >= ANSI_STACK_SIZE)
-    {
+    if (color_stack_ptr >= ANSI_STACK_SIZE)
         return;
-    }
-    ansi_color_stack[i++] = ansi_cur_color;
+    ansi_color_stack[color_stack_ptr++] = ansi_cur_color;
 }
 
 void restore_cursor_color()
 {
-    static sint i = 0;
-    if (i <= 0)
-    {
+    if (color_stack_ptr <= 0)
         return;
-    }
-    ansi_cur_color = ansi_color_stack[i--];
+    ansi_cur_color = ansi_color_stack[--color_stack_ptr];
     set_cursor_color(ansi_cur_color);
 }
